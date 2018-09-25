e340e9515c0a76a978b64b964b8280dd316915e1
Kevin Lau <laudevelopment@gmail.com>
2018-09-24 19:52:26 -0700

    Clean up directory and remove unneeded tests

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ YES = 1
 MAX_TEXT_LEN = 10 # max text length in repr
 TYPICAL_RESP_THRESHOLD = 0.5
 ANS_CONFIDENCE_THRESHOLD = 0.5
+MAX_DIFF_VAL = 1
 CONF_LVL_READY = 0
 CONF_LVL_SEARCHING = 1
 CONF_LVL_GUESS = -1
@@ -95,10 +96,7 @@ class Graph:
         self.order = []
         self.data = []
         self.size = 0
-        self.filtered_q = []
-        self.filtered_a = []
         self.tie_breaker = 0
-        self.keep_guessing = True
         
     def get_index(self, text_key):
         if text_key == CONF_LVL_GUESS:
@@ -231,7 +229,7 @@ class Graph:
             try:
                 value = abs(subtotal_point.ytoa() - TYPICAL_RESP_THRESHOLD)
             except TypeError:
-                value = 1
+                value = MAX_DIFF_VAL
             result.append((value, q_index))
         return result
 
@@ -252,8 +250,6 @@ class Graph:
 
         confidence_lvl = self.get_confidence_lvl(prev_len_potential_answers, potential_answers, history)
 
-        # if READY, ask answer but don't remove from potential answers
-        # leave that for game to handle
         if confidence_lvl == CONF_LVL_READY:
             node = self.get_node(potential_answers[0].pop())
         elif confidence_lvl == CONF_LVL_STOP:
@@ -269,77 +265,6 @@ class Graph:
             node = self.get_node(q_index)
 
         return (node, potential_answers, potential_questions)
-
-    def reset_filter(self, type_):
-        return [i for i in range(self.size) if self.get_node(i).type_ == type_]
-
-    def init_filtered_q(self):
-        self.filtered_q = self.reset_filter(Q)
-        return self.filtered_q
-
-    def init_filtered_a(self):
-        self.filtered_a = self.reset_filter(A)
-        return self.filtered_a
-
-    def update_filtered_q(self, entry):
-        self.filtered_q = self.filter_out(entry.index, self.filtered_q)
-        return self.filtered_q
-
-# TODO update so if match answers don't work, try for maybes
-    def update_filtered_a(self, entry):
-        match = []
-        maybe = []
-        for a_index in self.filtered_a:
-            point = self.get_point(entry.index, a_index)
-            if point.typical_resp() == entry.resp:
-                match.append(a_index)
-            elif point.typical_resp() is None:
-                maybe.append(a_index)
-        if len(match) == 0:
-            self.filtered_a = maybe
-        else:
-            self.filtered_a = match
-        
-    # calculates the yes to all answers ratio based on the remaining
-    # answers. for example, given a list of filtered_answers [0, 1, 2]
-    # and list of questions [3, 4, 5], return a list
-    # [None, None, None, %, %, %] where the % indicates that question's
-    # ratio of yes to all answers
-
-    def calc_ytoa(self, filtered_a, filtered_q):
-        subtotal_ytoa = [Point(0,0) for i in range(self.size)]
-        for a_index in filtered_a:
-            for q_index in filtered_q:
-                point = self.get_point(q_index, a_index)
-                index = q_index
-                subtotal_ytoa[index].inc(YES, point.yes)
-                subtotal_ytoa[index].inc(NO, point.no)
-        return [p.ytoa() for p in subtotal_ytoa]
-
-    def calc_dfrom50(self, array):
-        for i in range(len(array)):
-            try:
-                array[i] = abs(array[i] - TYPICAL_RESP_THRESHOLD)
-            except TypeError:
-                array[i] = None
-
-    def check_all_ans_same(self):
-        temp = []
-        if len(self.filtered_a) == 0:
-            print("empty filtered ans")
-            return True
-        for i in self.filtered_a:
-            temp.append(self.calc_ytoa([i], self.filtered_q))
-        for i in range(len(self.filtered_a)):
-            for j in range(i+1, len(self.filtered_a)):
-                if temp[i] != temp[j]:
-                    print(self.get_text(self.filtered_a[i]), temp[i])
-                    print(self.get_text(self.filtered_a[j]), temp[j])
-                    return False
-        return True
-
-    def get_ans_confidence(self):
-        return 1 if len(self.filtered_a) == 1 else 0
 
     def round_robin(self, array):
         self.tie_breaker = (self.tie_breaker + 1) % len(array)
@@ -359,59 +284,6 @@ class Graph:
             elif max_yes_count == point.yes:
                 max_list.append(a_index)
         return self.get_node(self.round_robin(max_list))
-
-    def next_question(self, history):
-        if len(history) == 0:
-            self.init_filtered_q()
-            self.init_filtered_a()
-        else:
-            self.update_filtered_a(history[-1])
-            self.update_filtered_q(history[-1])
-
-            if self.get_ans_confidence() > ANS_CONFIDENCE_THRESHOLD:
-                return self.get_best_ans()
-
-# consider checking whether all ans are same only if
-# there is good reason e.g. number of filtered ans
-# didn't decrease from previous iteration
-
-            all_ans_same = self.check_all_ans_same()
-            if all_ans_same is True:
-# guess based on % of times selected as answer.
-                if self.keep_guessing == True:
-                    self.keep_guessing = False
-                    print("guessing!")
-                    #a_index = self.round_robin(self.filtered_a)
-                    #return self.get_node(a_index)
-                    return self.get_best_ans()
-                if len(self.filtered_q) > 0:
-# if incorrect, ask if want to continue.
-# if no, add it to existing.
-# if yes, find (0, 1, None). 
-                    q_index = self.round_robin(self.filtered_q)
-                    return self.get_node(q_index)
-                else:
-                    print("You got us! No more questions from next question")
-                    return -1
-
-        subtotal = self.calc_ytoa(self.filtered_a, self.filtered_q)
-        self.calc_dfrom50(subtotal)
-        q_index = self.filtered_q[0]
-        q_dfrom50 = TYPICAL_RESP_THRESHOLD
-
-        min_list = [q_index]
-        for i in self.filtered_q:
-            if subtotal[i] is not None:
-                if subtotal[i] < q_dfrom50:
-                    q_dfrom50 = subtotal[i]
-                    q_index = i
-                    del(min_list[:])
-                    min_list.append(q_index)
-                elif subtotal[i] == q_dfrom50:
-                    min_list.append(i)
-
-        q_index = self.round_robin(min_list)
-        return self.get_node(q_index)
 
 class Game:
 
@@ -442,9 +314,8 @@ class Game:
         self.graph.add(A, answer)
         self.track_resp(self.graph.get_index(answer), A, YES)
 
-    def remove_incorrect_ans(self):
-        if self.history[-1].type_ == A:
-            print("Popped", self.history.pop())
+    def update_graph(self, history):
+        self.graph.update(history)
 
     def swap_last_two_entries(self):
         self.history[-1], self.history[-2] = self.history[-2], self.history[-1]
@@ -471,12 +342,11 @@ class Game:
                 print("Darn, we didn't get it")
                 self.ask_answer()
                 self.update_graph(self.history)
-                resp = get_ans("Play again?")
-                return resp
+                return NO
             elif node.text == CONF_LVL_WIN:
+                print("We got it! Play again?")
                 self.update_graph(self.history)
-                resp = get_ans("We got it! Play again?")
-                return resp
+                return NO
             elif node.text == CONF_LVL_GUESS:
                 resp = get_ans("Tricky...want me to guess?")
                 self.track_resp(CONF_LVL_GUESS, C, resp)
@@ -492,9 +362,6 @@ class Game:
             resp = get_ans(node.text)
             self.track_resp(self.graph.get_index(node.text), node.type_, resp)
             return YES
-
-    def update_graph(self, history):
-        self.graph.update(history)
 
     def save(self):
         if self.saved_file is None:
@@ -513,37 +380,23 @@ class Game:
     def resume_game(self):
         print("Loading game {}".format(self.saved_file))
 
-    def check_win(self):
-        if self.history[-1].resp == YES:
-            print("We guessed it!")
-            return True
-        elif self.history[-1].resp == NO:
-            print("Darn...we lost!")
-            self.ask_answer()
-            return True
-        else:
-            print("Error????", self.history)
-            return False
-
     def start(self):
         if (self.saved_file is None):
             self.new_game()
         else:
             self.resume_game()
 
-        keep_playing = self.ask_question()
-        while (keep_playing == YES):
-            keep_playing = self.ask_question()
-        del(self.history[:])
+        while (self.ask_question() == YES):
+            pass
 
-        print(self.graph.data, self.graph.order)
+        del(self.history[:])
         self.save()
 
     def stop(self):
         self.play = False
 
     def reset(self):
-        self.graph.keep_guessing = True
+        pass
 
     def prompt_replay(self):
         if get_ans("Replay (y/n)") == NO: 
